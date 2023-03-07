04da6b09cf5493bb31b8546482d0da6677f78bc7
vish <vishwanath5854@outlook.com>
2023-03-07 16:03:01 -0500

    Patch for small bug

--- models/form.js
+++ models/form.js
@@ -6,10 +6,6 @@ const ticketSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    password: {
-        type: String,
-        required: true
-    },
     email: {
         type: String,
         required: true,
