40d6e9bdc7e3f7a83968bd1f0d1a283f8cc0c074
vish <vishwanath5854@outlook.com>
2023-03-21 19:43:31 -0400

    Got rid of env stuff

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const PORT = 3000;
-export const MONGO_URL = "";
+export const MONGO_URL = process.env.MONGO_URL;
--- public/frontend/cs555-agile-titans/src/services/fetchService.js
+++ public/frontend/cs555-agile-titans/src/services/fetchService.js
@@ -1,10 +1,9 @@
 /** This is very painful for me to write something that uses callbacks, but REACT doesn't use async await */
 const fetchSomething = (url, requestOptions, successCb, errorCb) => {
-	url = process.env.REACT_APP_SERVER + url;
 	if (!Object.prototype.hasOwnProperty.call(requestOptions, "headers")) requestOptions.headers = new Headers();
-
 	requestOptions.headers.append("Auth", localStorage.getItem("Auth"));
-	fetch(url, requestOptions)
+
+	fetch(`${window.location.origin}/${url}`, requestOptions)
 		.then(res => {
 			res.text().then(resText => {
 				if (res.status === 401) return window.location.replace("/");
