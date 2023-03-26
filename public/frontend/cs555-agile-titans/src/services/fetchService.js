3d4a39075713ee9a37030172dc7762ace9dfe503
vish <vishwanath5854@outlook.com>
2023-03-26 16:27:36 -0400

    Fixed authentication crashing server

--- public/frontend/cs555-agile-titans/src/services/fetchService.js
+++ public/frontend/cs555-agile-titans/src/services/fetchService.js
@@ -6,7 +6,7 @@ const fetchSomething = (url, requestOptions, successCb, errorCb) => {
 	fetch(`${window.location.origin}/${url}`, requestOptions)
 		.then(res => {
 			res.text().then(resText => {
-				if (res.status === 401) return window.location.replace("/");
+				if ((res.status === 401) && (!url.includes("login"))) return window.location.replace("/");
 				if (!res.ok) return errorCb(JSON.parse(resText));
 
 				successCb(JSON.parse(resText));
