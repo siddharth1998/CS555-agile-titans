0e158b77b480c4fad3b8c5e153258c0de4d190ad
mickylab <42708694+mickylab@users.noreply.github.com>
2023-04-04 14:50:05 -0400

    Change success status code to 200

--- routes/contract.js
+++ routes/contract.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
   try {
     let firstParty = req.user.firstName + req.user.lastName;
     let contractList = await findContractByFirstParty(firstParty);
-    return res.status(201).json({ status: 'success', message: 'Get contract list successfully', contractList });
+    return res.status(200).json({ status: 'success', message: 'Get contract list successfully', contractList });
   } catch (err) {
     console.error(`Error while getting contract list`);
     console.error(err);
@@ -31,9 +31,9 @@ router.get("/countInProgress", async (req, res) => {
     let firstParty = req.user.firstName + req.user.lastName;
     let contractList = await findInProgressContracts(firstParty);
     let numberOfContracts = contractList.length;
-    return res.status(201).json({
+    return res.status(200).json({
       status: "success",
-      message: "Get contract list successfully",
+      message: "Get number of In Progress contracts successfully",
       numberOfContracts,
     });
   } catch (err) {
@@ -46,11 +46,11 @@ router.get("/countInProgress", async (req, res) => {
 /** /contract/create  */
 router.post("/create", async (req, res) => {
   try {
-    if (!req.body) res.status(400).json({ status : 'error', message : "No contract details are sent" });
+    if (!req.body) res.status(400).json({ status : 'error', message : "No contract list details are sent" });
     let contract = await createContractList(req.body);
-    return res.status(201).json({ status: 'success', message: 'Contract created successfully', contract });
+    return res.status(200).json({ status: 'success', message: 'Contract list created successfully', contract });
   } catch (err) {
-    console.error(`Error while creating a new contract`);
+    console.error(`Error while creating a new contract list`);
     console.error(err);
     return res.status(500).json({ status : 'error', message : err.message });
   }
@@ -61,7 +61,7 @@ router.get("/details/:contractNo", async (req, res) => {
   try {
     let contractNo = req.params.contractNo;
     let contractContent = await getContractContentByContractNo(contractNo);
-    return res.status(201).json({ status: 'success', message: 'Get contract content successfully', contractContent });
+    return res.status(200).json({ status: 'success', message: 'Get contract content successfully', contractContent });
   } catch (err) {
     console.error(`Error while getting contract content`);
     console.error(err);
@@ -76,9 +76,9 @@ router.post("/details/:contractNo", async (req, res) => {
     let secondParty = req.body.secondPartySignature;
     await updateSecondPartySignature(contractNo, secondParty);
     let updatedContent = await updateSecondParty(contractNo, secondParty);
-    return res.status(201).json({
+    return res.status(200).json({
       status: "success",
-      message: "Get contract content successfully",
+      message: "Update contract second party signature successfully",
       updatedContent,
     });
   } catch (err) {
@@ -100,7 +100,7 @@ router.post("/details/create", async (req, res) => {
       req.body.endDate,
       req.body.dateSigned,
     );
-    return res.status(201).json({ status: 'success', message: 'Contract content created successfully', contract });
+    return res.status(200).json({ status: 'success', message: 'Contract content created successfully', contract });
   } catch (err) {
     console.error(`Error while creating the contract content`);
     console.error(err);
