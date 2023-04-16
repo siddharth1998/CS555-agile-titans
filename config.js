f3a270dfd7a3feed0d374a6ef9ccd0f393cde7b5
Pradyumn Pundir <pundirpradyumn25@gmail.com>
2023-04-15 20:08:48 -0400

    db cred push

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const PORT = process.env.PORT || 3000;
-export const MONGO_URL = "mongodb+srv://z:RG99XAwjOJsDJijG@agile.ctke1it.mongodb.net/agile?retryWrites=true&w=majority";
+export const MONGO_URL = process.env.MONGO_URL;
