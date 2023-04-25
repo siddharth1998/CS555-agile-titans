58f3e7057802cd4ac107eeb3c2a9bfc0a1c18b49
siddharth1998 <siddharth1998@gmail.com>
2023-04-24 20:52:39 -0400

    refactored

--- routes/photoUpload.js
+++ routes/photoUpload.js
@@ -10,7 +10,7 @@ let storage = multer.diskStorage({
         if (!req.count) req.count = 1;
 
         let extension = file.originalname.split('.')[1];
-        req.count ++;
+        req.count++;
         return cb(null, `${Date.now()}${req.count}.${extension}`);
     }
 });
@@ -19,48 +19,48 @@ const upload = multer({ storage })// const multer  = require('multer')
 
 const router = express.Router();// console.log("Reached before the router post photo upload");
 
-router.post("/",upload.array('imageFile', 20),async (req,res)=>{
+router.post("/", upload.array('imageFile', 20), async (req, res) => {
     const file = req.files;
 
     let fileNames = req.files.map(f => f.filename);
 
-    const result_json= new photoUploadModel({
-        customerID:req.body.customerId,
+    const result_json = new photoUploadModel({
+        customerID: req.body.customerId,
         fileNames: fileNames,
         content: req.body.comment,
         email: req.body.email,
         operation: req.body.operation
     });
-   
-    let variable= result_json._id.toString()
+
+    let variable = result_json._id.toString()
     await result_json.save();
     return res.redirect("photoUpload/dashboard");
 }
 );
 
-router.get("/changeStatus/:id", async(req, res) => {
+router.get("/changeStatus/:id", async (req, res) => {
     if (!req.params.id) {
         return res.redirect("photoUpload/dashboard");
-    } 
+    }
 
     let caseId = req.params.id;
-    const file = await photoUploadModel.find({_id: caseId});
+    const file = await photoUploadModel.find({ _id: caseId });
 
     if (caseId) return res.render('PhotoInspection/changePhoto.ejs', { requests: file });
 
     return res.render("PhotoInspection/create")
 })
 
-router.get("/",upload.single('image'),async (req,res)=>{
-    return res.render("PhotoInspection/createPhoto.ejs"); 
+router.get("/", upload.single('image'), async (req, res) => {
+    return res.render("PhotoInspection/createPhoto.ejs");
 }
 );
 
-router.get("/dashboard", async (req,res)=>{
+router.get("/dashboard", async (req, res) => {
 
     try {
         const file = await photoUploadModel.find();
-        return res.render("PhotoInspection/photoDashboard.ejs", { requests: file}); 
+        return res.render("PhotoInspection/photoDashboard.ejs", { requests: file });
     } catch (err) {
         console.error(err);
         return res.status(500).send("Server Error");
