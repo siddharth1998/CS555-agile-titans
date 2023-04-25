688acac0f091fb1629ccbdf1d57354803e600e18
desai-aayushi <aayushi1512000@gmail.com>
2023-04-25 01:10:29 -0400

    Project Edit

--- routes/Project.js
+++ routes/Project.js
@@ -1,8 +1,143 @@
 import express from "express";
 import { projectModel } from "../models/project.js";
 import { taskModel } from "../models/Task.js";
+import { createObjectCsvWriter } from "csv-writer";
+import { promisify } from "util";
+import fs from "fs";
+//import { createCanvas } from "canvas";
+//const Chart = require("chart.js");
 
 const router = express.Router();
+
+async function createPieChartImage() {
+  // create a canvas to draw the chart on
+  const canvas = createCanvas(400, 400);
+  const ctx = canvas.getContext("2d");
+
+  // define the data for the chart
+  const data = {
+    labels: ["Completed", "In Progress", "Not Started"],
+    datasets: [
+      {
+        data: [20, 30, 50],
+        backgroundColor: ["#36A2EB", "#FFCE56", "#FF6384"],
+      },
+    ],
+  };
+
+  // create a new pie chart and draw it on the canvas
+  const chart = new Chart(ctx, {
+    type: "pie",
+    data: data,
+  });
+
+  // get the image data from the canvas
+  const imageData = canvas.toBuffer();
+}
+
+async function downloadCSVData(data) {
+  // Create a new CSV writer
+  const writer = createObjectCsvWriter({
+    path: "data.csv",
+    header: Object.keys(data[0]).map((key) => ({ id: key, title: key })),
+  });
+
+  // Write the data to the CSV file
+  await writer.writeRecords(data);
+
+  // Read the CSV file into a buffer
+  const buffer = await promisify(fs.readFile)("data.csv");
+
+  // Return the buffer as a downloadable file
+  return {
+    filename: "data.csv",
+    data: buffer,
+  };
+}
+
+router.get("/download", async (req, res) => {
+  try {
+    const projects = await projectModel.find();
+    // Filter tasks by status
+    const completedProjects = projects.filter(
+      (project) => project.status === "Completed"
+    );
+
+    // Calculate completed task count
+    const completedProjectCount = completedProjects.length;
+
+    // Calculate remaining task count
+    const remainingProjectCount = projects.length - completedProjectCount;
+
+    // Create pie chart data
+    const pieChartData = [
+      { name: "Completed Projects", value: completedProjectCount },
+      { name: "Remaining Projects", value: remainingProjectCount },
+    ];
+    const data = projects.map((project) => {
+      return {
+        projectName: project.projectName,
+        description: project.description,
+        status: project.status,
+        endDate: project.endDate,
+      };
+    });
+    const file = await downloadCSVData(data);
+    // Create pie chart image
+    const pieChartImage = await createPieChartImage(pieChartData);
+
+    // Create zip file containing CSV file and pie chart image
+    const zip = new JSZip();
+    zip.file(filename, data);
+    zip.file("pie-chart.png", pieChartImage, { binary: true });
+
+    // Send zip file as response
+    const zipData = await zip.generateAsync({ type: "nodebuffer" });
+
+    res.setHeader("Content-Type", "application/zip");
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename=${file.filename}`
+    );
+
+    res.send(zipData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+//Backend API for downloading task data in csv, using downloadCSVData function
+router.get("/:id/tasks/download", async (req, res) => {
+  try {
+    const projectId = req.params.id;
+    const tasks = await taskModel
+      .find({ project: projectId })
+      .populate("taskAssign");
+
+    const data = tasks.map((task) => {
+      return {
+        taskName: task.taskName,
+        taskDescription: task.Description,
+        taskPriority: task.priority,
+        taskAssign: task.taskAssign.firstName,
+        taskDueDate: task.dueDate,
+        taskStatus: task.status,
+      };
+    });
+    console.log("data::::::", data);
+    const file = await downloadCSVData(data);
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename=${file.filename}`
+    );
+    res.send(file.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
 
 /*get task based on project id */
 router.get("/:projectId/task", async (req, res) => {
@@ -31,7 +166,6 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  console.log(req.body);
   try {
     const newProject = new projectModel({
       projectName: req.body.projectName,
@@ -71,7 +205,17 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.put("/projects/:id", async (req, res) => {
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const project = await projectModel.findById(req.params.id);
+    res.render("Dashboard/ProjectEdit.ejs", { project });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.post("/edit/:id", async (req, res) => {
   try {
     const project = await projectModel.findByIdAndUpdate(
       req.params.id,
@@ -85,10 +229,10 @@ router.put("/projects/:id", async (req, res) => {
           teamMembers: req.body.teamMembers,
         },
       },
-      { new: true }
+      { new: req.body.new }
     );
 
-    res.render("Dashboard/Tasks.ejs", { project });
+    res.render("Dashboard/ProjectDetails.ejs", { project });
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
